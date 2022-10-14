17815af8797752bb869886c3cab07234e198fe2b
flexypianist <ibehafoke@gmail.com>
2022-10-14 03:42:20 -0700

    printfc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - prints the phrase with proper grammar,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
